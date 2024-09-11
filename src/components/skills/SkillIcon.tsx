06ce577ad1aa42bf06a33e5adb8140e29b5c4ac6
SanghyunLee <lgw9736@naver.com>
2024-09-11 17:21:34 +0900

    💄 skillIcon 추가

---
+++ src/components/skills/SkillIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from "next/image";
+
+type Props = {
+    skillName: string
+}
+
+export default function SkillIcon({ skillName }: Props) {
+    return (
+        <Image
+            key={"html5"}
+            className="rounded-sm mx-1"
+            src={`/icons/${skillName}.svg`}
+            alt={"stack"}
+            priority
+            quality={100}
+            width={50}
+            height={50}
+            style={{ width: 50, height: 50 }}
+        />
+    )
+}
