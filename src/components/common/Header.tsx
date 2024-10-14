0bd9f9303c5ffa66689a25369010d39ad5f22c62
SanghyunLee <lgw9736@naver.com>
2024-10-14 14:49:18 +0900

    feat : 기본 뼈대 적용

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Header({}: Props) {
+
+  return (
+    <div>
+        ABOUT ME, SKILLS, PROJECTS, ACTIVITY, CONTACT
+    </div>
+  )
+}
