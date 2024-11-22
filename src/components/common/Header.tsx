0ce8eb6c357ce8e8ac3bbcb751d0b923c41e799a
SanghyunLee <lgw9736@naver.com>
2024-11-22 19:53:31 +0900

    feat: Header 추가
    - 클릭하면 id로 이동

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,14 +3,25 @@ import React from 'react';
 type Props = {};
 
 export default function Header({}: Props) {
-  const navLinks = ['ABOUT ME', 'SKILLS', 'ACTIVITY', 'CONTACT'];
+  const navLinks = ['ABOUT', 'SKILLS', 'ACTIVITY', 'CONTACT'];
 
   return (
-    <div>
-      <span>idealHyun</span>
+    <header className="flex items-center justify-between p-4 bg-gray-800 text-white w-full">
+      <span className="text-xl font-bold">idealHyun</span>
       <nav>
-        <ul>{/*{navLinks.map(link => ())}*/}</ul>
+        <ul className="flex space-x-4">
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <a
+                href={`#${link.replace(/\s+/g, '').toLowerCase()}`}
+                className="hover:text-blue-400"
+              >
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
-    </div>
+    </header>
   );
 }
