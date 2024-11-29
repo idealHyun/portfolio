2e7e3fae1a0de95ad72ce999d2f9d3a1be0e70f6
SanghyunLee <lgw9736@naver.com>
2024-11-29 18:23:19 +0900

    chore: 프로젝트 섹션 수정
    - 프로젝트에 사진, 제목, 간단한 설명, 기술 스택, 개인/팀 프로젝트 여부에 대해 표시할 예정

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import Image from 'next/image';
+import { ProjectType } from '../../../types/projectType';
+import { soulMBTI } from '../../../data/projects/soulMBTI';
+import TechStack from '@/components/projects/TechStack';
 
 export default function Project() {
-  return <div>Project</div>;
+  const projects: ProjectType[] = [soulMBTI];
+
+  return (
+    <div className="flex justify-center items-center w-full border-2 rounded-xl overflow-hidden">
+      {/* Project Thumbnail */}
+      <Image
+        src="/images/about/취업사진2.jpg"
+        alt="Project"
+        width={150}
+        height={200}
+      />
+      {/* Project Content */}
+      <div className="p-4 flex flex-col items-start h-full">
+        <span className="inline-block bg-blue-400 rounded-lg p-1 w-auto">
+          Team
+        </span>
+        <span className="text-2xl">SOUL MBTIE</span>
+        <span>MBTI와 관심사를 기반으로한 교내 매칭 어플</span>
+
+        <div className="flex gap-1 items-center">
+          <TechStack techName="dart" />
+          <TechStack techName="flutter" />
+          <TechStack techName="firebase" />
+        </div>
+
+        <button className="bg-gray-800 rounded-lg p-1 text-white">
+          README
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -1,9 +1,11 @@
 import Title from '@/components/common/Title';
+import Project from '@/components/projects/Project';
 
 export default function ProjectSection() {
   return (
     <section id="projects" className="grid">
       <Title title="PROJECTS" />
+      <Project />
     </section>
   );
 }
