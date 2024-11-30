23e9af029f6cf97430bd84ef9c7f12e08f2102c2
SanghyunLee <lgw9736@naver.com>
2024-11-30 18:57:12 +0900

    chore: Project 수정
    - 날짜 추가

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -6,8 +6,8 @@ import { ProjectType } from '../../../types/projectType';
 export default function Project({ project }: { project: ProjectType }) {
   return (
     <div
-      key={project.title} // 각 요소에 고유 key 필요
-      className="flex justify-center items-center w-full border-2 rounded-xl overflow-hidden"
+      key={project.title}
+      className="flex w-full justify-center items-center border-2 rounded-xl overflow-hidden"
     >
       {/* Project Thumbnail */}
       <Image
@@ -17,20 +17,27 @@ export default function Project({ project }: { project: ProjectType }) {
         height={200}
       />
       {/* Project Content */}
-      <div className="p-4 flex flex-col items-start h-full">
-        <span className="inline-block bg-blue-400 rounded-lg p-1 w-auto">
-          {project.category}
-        </span>
-        <span className="text-2xl">{project.title}</span>
-        <span>{project.subTitle}</span>
+      <div className="flex h-full flex-col justify-between items-start px-4 py-2">
+        <div>
+          <span className="inline-block w-auto rounded-lg bg-blue-400 p-1">
+            {project.category}
+          </span>
+        </div>
+        <div className="flex">
+          {project.startDate} ~ {project.endDate}
+        </div>
+        <div>
+          <div className="text-2xl">{project.title}</div>
+          <div>{project.subTitle}</div>
+        </div>
 
-        <div className="flex gap-1 items-center">
+        <div className="flex items-center gap-1 pb-1">
           {project.techStacks.map((techName: string) => (
             <TechStack key={techName} techName={techName} />
           ))}
         </div>
 
-        <button className="bg-gray-800 rounded-lg p-1 text-white">
+        <button className="rounded-lg bg-gray-800 p-1 text-white">
           README
         </button>
       </div>
