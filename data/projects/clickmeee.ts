46a27812054b4252fabb70b31003348390f17356
SanghyunLee <lgw9736@naver.com>
2024-12-30 22:29:54 +0900

    chore: clickmeee 데이터 추가

---
+++ data/projects/clickmeee.ts
@@ -0,0 +1,14 @@
+import { ProjectType } from '../../types/projectType';
+
+export const clickmeee: ProjectType = {
+  title: 'Click Meee',
+  mdFilePath: '/markdown/clickmeee.mdx',
+  subTitle: '클릭을 통해 다른 사용자들과 대결하는 웹 어플리케이션',
+  startDate: '2024.12',
+  endDate: '',
+  category: 'Team',
+  techStacks: ['typescript', 'react', 'vite', 'tailwindcss', 'recoil'],
+  githubUrl: 'https://github.com/ClickMeee',
+  images: [],
+  thumbNail: '/images/projects/clickmeee/clickmeee.svg',
+};
