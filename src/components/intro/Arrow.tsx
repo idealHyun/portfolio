5b51f78e47269d1ce67eb94cdbec52da03238251
SanghyunLee <lgw9736@naver.com>
2025-01-01 17:53:27 +0900

    fix: 화살표 수정

---
+++ src/components/intro/Arrow.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+const Arrow = () => {
+  return (
+    <div className="flex items-center justify-center bg-gray-950 mt-40">
+      <div className="bottom-0 left-1/2 transform -translate-x-1/2 w-10 h-10 bg-contain bg-no-repeat animate-bounce">
+        <Image alt="화살표" src="/icons/arrow.svg" width={80} height={80} />
+      </div>
+    </div>
+  );
+};
+
+export default Arrow;
