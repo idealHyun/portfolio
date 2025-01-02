196d2abf495fd310e6dcd483ec1040b79bfacc03
SanghyunLee <lgw9736@naver.com>
2025-01-02 14:18:04 +0900

    chore: 프로젝트 데이터 추가

---
+++ data/projects/GongmoHub.ts
@@ -0,0 +1,14 @@
+import { ProjectType } from '../../types/projectType';
+
+export const gongmoHub: ProjectType = {
+  title: 'GongmoHub',
+  mdFilePath: '/markdown/gongmoHub.mdx',
+  subTitle: '텍스트 설명을 통한 공모전 추천 플랫폼',
+  startDate: '2024.10.07',
+  endDate: '2024.10.09',
+  category: 'Team',
+  techStacks: ['typescript', 'next', 'sass', 'antd', 'figma'],
+  githubUrl: 'https://github.com/GongmoHub',
+  images: [],
+  thumbNail: '',
+};
--- data/projects/clickmeee.ts
+++ data/projects/clickmeee.ts
@@ -5,10 +5,11 @@ export const clickmeee: ProjectType = {
   mdFilePath: '/markdown/clickmeee.mdx',
   subTitle: '클릭을 통해 다른 사용자들과 대결하는 웹 어플리케이션',
   startDate: '2024.12',
-  endDate: '',
+  endDate: '진행 중',
   category: 'Team',
   techStacks: ['typescript', 'react', 'vite', 'tailwindcss', 'recoil'],
   githubUrl: 'https://github.com/ClickMeee',
   images: [],
   thumbNail: '/images/projects/clickmeee/clickmeee.svg',
+  serviceUrl: 'https://클릭해줘.메인.한국',
 };
