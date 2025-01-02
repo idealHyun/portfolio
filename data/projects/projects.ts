be0bccb493c753291044f83125ceea42630f4a01
SanghyunLee <lgw9736@naver.com>
2025-01-02 14:17:05 +0900

    feat: 프로젝트 연도별로 정리

--- data/projects/projects.ts
+++ data/projects/projects.ts
@@ -4,12 +4,14 @@ import { ERP } from './ERP';
 import { gukbab } from './gukbab';
 import { smileHub } from './smileHub';
 import { clickmeee } from './clickmeee';
+import { portfolio } from './portfolio';
+import { gongmoHub } from './GongmoHub';
 
 type ProjectsByYear = {
   [year: string]: ProjectType[];
 };
 
 export const projects: ProjectsByYear = {
-  '2024': [clickmeee, smileHub, soulMBTI],
+  '2024': [clickmeee, smileHub, soulMBTI, portfolio, gongmoHub],
   '2023': [ERP, gukbab],
 };
--- src/components/projects/ProjectSection.tsx
+++ src/components/projects/ProjectSection.tsx
@@ -8,37 +8,47 @@ export default function ProjectSection() {
       <Title title="PROJECTS" />
       {Object.entries(projects)
         .sort(([yearA], [yearB]) => Number(yearB) - Number(yearA))
-        .map(([year, yearProjects]) => (
-          <div key={year}>
-            <span className="flex justify-center my-2 text-xl font-bold">
-              {year}
-            </span>
+        .map(([year, yearProjects]) => {
+          const sortedProjects = yearProjects.sort(
+            (a, b) =>
+              new Date(b.startDate).getTime() - new Date(a.startDate).getTime()
+          );
 
-            {/* 프로젝트 영역 */}
-            <div className="grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr] gap-4 lg:gap-8 h-full">
-              {/* 왼쪽 프로젝트 */}
-              <div className="space-y-8">
-                {yearProjects
-                  .filter((_, index) => index % 2 === 0) // 짝수 인덱스 (왼쪽)
-                  .map((project) => (
+          const leftProjects = sortedProjects.filter(
+            (_, index) => index % 2 === 0
+          );
+          const rightProjects = sortedProjects.filter(
+            (_, index) => index % 2 === 1
+          );
+
+          return (
+            <div key={year}>
+              <span className="flex justify-center my-2 text-xl font-bold">
+                {year}
+              </span>
+
+              {/* 프로젝트 영역 */}
+              <div className="grid grid-cols-1 lg:grid-cols-[1fr_auto_1fr] gap-4 lg:gap-8 h-full">
+                {/* 왼쪽 프로젝트 */}
+                <div className="space-y-8">
+                  {leftProjects.map((project) => (
                     <Project key={project.title} project={project} />
                   ))}
-              </div>
+                </div>
 
-              {/* 세로 선 */}
-              <div className="hidden lg:block w-1 bg-gray-950"></div>
+                {/* 세로 선 */}
+                <div className="hidden lg:block w-1 bg-gray-950"></div>
 
-              {/* 오른쪽 프로젝트 */}
-              <div className="lg:mt-20 space-y-8">
-                {yearProjects
-                  .filter((_, index) => index % 2 === 1) // 홀수 인덱스 (오른쪽)
-                  .map((project) => (
+                {/* 오른쪽 프로젝트 */}
+                <div className="lg:mt-20 space-y-8">
+                  {rightProjects.map((project) => (
                     <Project key={project.title} project={project} />
                   ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
     </section>
   );
 }
